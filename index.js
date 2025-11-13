7cf62d194cc575f2261f0845cc5a11d514d1739b
Andrea Benz <andrealevin.benz@gmail.com>
2025-11-13 11:50:28 +0100

    added picture uploads

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const { PrismaClient, categories } = require('./generated/prisma');
+const fileUpload = require('express-fileupload');
+const fs = require('fs').promises;
+const { v4: uuidv4 } = require('uuid')
 
 const app = express();
 const prisma = new PrismaClient();
@@ -15,6 +18,11 @@ app.set('views', path.join(__dirname, 'src/views'));
 app.use(expressLayouts);
 app.set('layout', 'layout');
 app.use(express.urlencoded({ extended: true }));
+app.use(fileUpload({
+    createParentPath: true,
+    limits: { filesize: 10*1024*1024}
+}))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 // Routes
@@ -72,7 +80,33 @@ app.post('/posts', async (req, res) => {
         return res.status(400).send('Ungültige Kategorie');
     }
 
-    await prisma.post.create({ data: {title, category, description} });
+    const newPost = await prisma.post.create({ data: {title, category, description} });
+
+    const files = req.files && req.files.images ? req.files.images : null;
+    if (files) {
+        const filesArray = Array.isArray(files) ? files : [files];
+        const postFolder = path.join(__dirname, 'uploads', 'posts', String(newPost.id));
+        await fs.mkdir(postFolder, {recursive: true});
+
+        const imageData = [];
+
+        for (const file of filesArray) {
+            if (!file.mimetype.startsWith('image/')) continue;
+
+            const ext = path.extname(file.name);
+            const filename = `${uuidv4()}${ext}`;
+            const destPath = path.join(postFolder, filename);
+            await file.mv(destPath);
+
+            const publicPath = path.posix.join('/uploads', 'posts', String(newPost.id), filename);
+            imageData.push({ path: publicPath, postId: newPost.id });
+        }
+
+        if (imageData.length > 0) {
+            await prisma.image.createMany({ data: imageData })
+        }
+    }
+
     res.redirect('/posts');
 })
 
