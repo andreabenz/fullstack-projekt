099620d14d02af90a2b4d421d7ff7fcc1825c9c4
Andrea Benz <andrealevin.benz@gmail.com>
2025-12-02 16:23:13 +0100

    removed tabs tabs library due to issue

--- public/scripts/tabs.js
+++
@@ -1,4 +0,0 @@
-import '@spectrum-web-components/tabs/sp-tabs.js';
-import '@spectrum-web-components/tabs/sp-tab.js';
-import '@spectrum-web-components/tabs/sp-tab-panel.js';
-import '@spectrum-web-components/theme/sp-theme.js';
