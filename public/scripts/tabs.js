6bab0881c361508b5fa03ba43bbbcbfd8832143d
Andrea Benz <andrealevin.benz@gmail.com>
2025-12-02 15:46:23 +0100

    added spectrum web component sp-tabs

---
+++ public/scripts/tabs.js
@@ -0,0 +1,4 @@
+import '@spectrum-web-components/tabs/sp-tabs.js';
+import '@spectrum-web-components/tabs/sp-tab.js';
+import '@spectrum-web-components/tabs/sp-tab-panel.js';
+import '@spectrum-web-components/theme/sp-theme.js';
